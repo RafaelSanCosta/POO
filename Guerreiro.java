2eb926f928165be15298255ac815fb786e3ded8f
Rafael Santos Costa <rafaelscosta71@gmail.com>
2025-01-26 16:45:18 -0300

    super nos construtores e no metodo

--- Guerreiro.java
+++ Guerreiro.java
@@ -2,15 +2,16 @@ public class Guerreiro extends Personagem{
     String arma;
     
     public Guerreiro (String nome, int saude, int nivel, String arma){
-        this.nome = nome;
-        this.saude = saude;
-        this.nivel = nivel;
+        super(nome, saude, nivel);
         this.arma = arma;
     }
 
-    public Guerreiro(){}
+    public Guerreiro(){
+        super();
+    }
 
     public void mostrarInfo(){
-        System.out.println("Nome: " + nome + ", Nível: " + nivel + ", Saúde: " + saude + ", Arma: " + arma);
+        super.mostrarInfo();
+        System.out.print("Arma: "+ arma);
     }
 }
